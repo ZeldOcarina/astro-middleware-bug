65e7cc9e24f76d8f976f971838472fc1ac5595ba
ZeldOcarina <mattia.r86@gmail.com>
2023-08-23 18:51:27 +0200

    Add node

---
+++ dist/server/chunks/index@_@astro.f5c9b745.mjs
@@ -0,0 +1,17 @@
+export { renderers } from '../renderers.mjs';
+export { onRequest } from '../_empty-middleware.mjs';
+import './astro.15ede2a8.mjs';
+import 'html-escaper';
+import 'path-to-regexp';
+import 'cookie';
+import '@astrojs/internal-helpers/path';
+import 'kleur/colors';
+import 'node:fs';
+import 'node:http';
+import 'node:tls';
+import 'mime';
+import 'string-width';
+
+const page = () => import('./pages/index.astro.0fc7e4ea.mjs');
+
+export { page };
